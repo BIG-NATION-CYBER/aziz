9d94a85823fa4299b52e84d45412166927277df0
BIG-NATION-CYBER <ramadhanasha553@gmail.com>
2026-06-05 15:18:09 +0300

    Add files via upload

---
+++ aziz/src/main/java/com/example/brothers/BrothersApplication.java
@@ -0,0 +1,13 @@
+package com.example.brothers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrothersApplication {
+
+    public static <String> void main(String[] args) {
+        SpringApplication.run(BrothersApplication.class, args);
+    }
+
+}
